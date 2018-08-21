c3d6eb5d232237986f4442bc74b464b3fe8e7cc3
Brandon Johnson <brandon.johnson@rcu.org>
2018-08-21 09:59:10 -0500

    bundle the compiled JS into one file

--- 01-hello-canvas/js/bundle.js
+++ 01-hello-canvas/js/bundle.js
@@ -1,3 +1,34 @@
+var graphics = (function () {
+    "use strict";
+    function createCanvas(width, height, id) {
+        var canvas = document.createElement("canvas");
+        canvas.width = width;
+        canvas.height = height;
+        if (id)
+            canvas.id = id;
+        return canvas;
+    }
+    function scale(canvas, scale) {
+        canvas.style.width = canvas.width * scale + "px";
+        canvas.style.height = canvas.height * scale + "px";
+    }
+    function loadImage(src) {
+        var img = new Image();
+        var p = new Promise(function (resolve) {
+            img.onload = function () {
+                resolve(img);
+            };
+        });
+        img.src = src;
+        return p;
+    }
+    return {
+        createCanvas: createCanvas,
+        scale: scale,
+        loadImage: loadImage
+    };
+}());
+/// <reference path="graphics.ts" />
 (function () {
     "use strict";
     var lastTime; //timestamp of last game loop iteration
@@ -55,4 +86,4 @@
         return frames[animation[number]];
     }
 }());
-//# sourceMappingURL=main.js.map
+//# sourceMappingURL=bundle.js.map
